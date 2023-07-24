bdf5b67b0ceb5a93b9dd212dccb54de373b078c3
xormetric <xormetric@tentacle.expert>
2023-07-24 17:08:00 -0400

    initial commit

---
+++ replaceable.js
@@ -0,0 +1,17 @@
+import { asyncReplace } from 'lit-html/directives/async-replace.js';
+
+export function replaceable(first) {
+	let f;
+
+	async function* generator() {
+		yield first;
+
+		while (true) {
+			yield await new Promise(r => {
+				f = v => r(v);
+			});
+		}
+	}
+
+	return [asyncReplace(generator()), v => f(v)]
+}
